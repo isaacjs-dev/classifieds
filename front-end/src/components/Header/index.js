059d9894fd316dc78eb01dec1ff1f479857256e1
Isaac Silvério <isaacjs.dev@gmail.com>
2020-10-19 21:22:48 -0300

    Creating the application header

--- front-end/src/components/Header/index.js
+++ front-end/src/components/Header/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import { HeaderArea } from './styled'
 
 const Header = () => {
   
   return (
-    <HeaderArea>Header Page</HeaderArea>
+    <HeaderArea>
+      <div className="container">
+        <div className="logo">
+          <Link to="/">
+            <span className="letter-01">O</span>
+            <span className="letter-02">DEV</span>
+            <span className="letter-03">X</span>
+          </Link>
+        </div>
+        <nav>
+          <ul>
+            <li><Link to="">Login</Link></li>
+            <li><Link to="">Cadastrar</Link></li>
+            <li><Link to="" className='button'>Anúncie Aqui</Link></li>
+          </ul>
+        </nav>
+      </div>
+    </HeaderArea>
   );
 }
 
--- front-end/src/components/Header/styled.js
+++ front-end/src/components/Header/styled.js
@@ -1,6 +1,86 @@
 import styled from 'styled-components';
 
 export const HeaderArea = styled.div`
-    height: 100px;
-    background-color: #CCC;
+    background-color: #FFF;
+    height: 60px;
+    border-bottom: 1px solid #CCC;
+
+    .container {
+        max-width: 1000px;
+        margin: auto;
+        display: flex;
+
+        a {
+            text-decoration: none;
+        }
+
+        .logo {
+            flex: 1;
+            display: flex;
+            align-items: center;
+            height: 60px;
+            transform: rotate(-10deg);
+            margin-top:-50px;
+
+
+            .letter-01,
+            .letter-02,
+            .letter-03 {
+                font-size: 32px;
+                font-weight: 800;
+            }
+            .letter-01 {
+                color: #F00;
+               
+            }
+            .letter-02 {
+                color: #0F0;
+                font-size: 24px;
+                margin-left: -15px;
+                margin-right: -15px;
+            }
+            .letter-03 {
+                color: #00F;
+                margin-top: -15px;
+            }
+        }
+
+        nav {
+            padding: 10px 0;
+
+            ul, li {
+                margin: 0;
+                padding: 0;
+                list-style: none;
+            }
+
+            ul {
+                display: flex;
+                align-items: center;
+                height: 40px;
+            }
+
+            li {
+                margin: 0 20px;
+
+                a {
+                    color: #000;
+                    font-size: 14px;
+
+                    &:hover {
+                        color: #999;
+                    }
+
+                    &.button {
+                        background-color: #FF8100;
+                        border-radius: 4px;
+                        color: #FFF;
+                        padding: 5px 10px;
+                    }
+
+                    &.button:hover { background-color: #E57706}
+                }
+            }
+        }
+    }
 `;
